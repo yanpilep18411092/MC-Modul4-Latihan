5dd096c21c3ed72028695aabe13125ea19dba988
Yan Pilep <yanpilep18411092@gmail.com>
2025-05-16 13:22:39 +0900

    Modul-4 Latihan 2nd commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,23 +34,21 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
-
     buildFeatures {
         viewBinding = true
     }
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.16.0")
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.activity:activity:1.10.1")
-    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
-    implementation("androidx.compose.material3:material3-android:1.3.2")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.6.0")
-    implementation("androidx.navigation:navigation-ui-ktx:2.6.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.2.1")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.material3.android)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
 }
--- app/src/main/java/com/example/projectserwin/MainActivity.kt
+++ app/src/main/java/com/example/projectserwin/MainActivity.kt
@@ -11,9 +11,7 @@ import com.example.projectserwin.databinding.ActivityMainBinding
 const val EXTRA_MESSAGE = " "
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding:ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -24,8 +22,6 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-
         binding.btnFirst.setOnClickListener() {
             val intent = Intent(this, SecondActivity::class.java)
             intent.putExtra("EXTRA_MESSAGE", binding.editPesan.text.toString())
